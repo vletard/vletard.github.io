1f482238592ff2dbf47a52418b6e53ff47b8ba65
vincent letard <vincent.letard@laposte.net>
2018-01-16 09:31:35 +0100

    Premier commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<html>
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <title>650 Site too awesome</title>
+  </head>
+  <body>
+  <h1>650 Site too awesome</h1>
+  This site is too awesome to be displayed on your screen.
+
+  </body>
+</html>
